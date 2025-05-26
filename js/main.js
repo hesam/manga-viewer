9325150c7cec0d409be591373f860a136ec185de
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-05-26 00:07:44 +0000

    up

--- js/main.js
+++ js/main.js
@@ -1,33 +1,88 @@
 'use strict';
 const numImages = 10;
-const boxFadeDelay = 500;
+const boxFadeDelay = 350;
 // Adjust delay to match typical double-click speed:
 const singleClickDelay = 250;
 const $boxImage = document.querySelector('.box');
 if (!$boxImage) throw new Error('Image not found!');
-let currentIndex = 1;
+let currIndex = 1;
 let singleClickTimeout = null;
+let fadeStepTimeout = null;
+const numFadeSteps = 25;
+let currFadeStep = 0;
+const opacityMax = 1;
+const opacityMin = 0;
+let opacity = opacityMax;
+const scaleMax = 1.05;
+const scaleMin = 1;
+let scale = 1;
 function showIndex(targetIndex) {
   console.log(targetIndex);
-  currentIndex = targetIndex;
-  $boxImage?.classList.add('faded-out');
-  setTimeout(() => {
-    $boxImage.setAttribute('src', `images/${currentIndex}.png`);
-    $boxImage?.classList.remove('faded-out');
-  }, boxFadeDelay);
+  currIndex = targetIndex;
+  /*
+    $boxImage?.classList.add('faded-out');
+    setTimeout(() => {
+      $boxImage!.setAttribute('src', `images/${currIndex}.png`);
+      $boxImage?.classList.remove('faded-out');
+    }, boxFadeDelay);
+    */
+  performFadeOut();
+}
+function performFadeOut() {
+  performFadeInOut(opacityMax, opacityMin, scaleMin, scaleMax, true, false);
+}
+function performFadeIn(firstTime) {
+  performFadeInOut(
+    opacityMin,
+    opacityMax,
+    scaleMax,
+    scaleMin,
+    false,
+    firstTime,
+  );
+}
+function performFadeInOut(
+  opacityStart,
+  opacityEnd,
+  scaleStart,
+  scaleEnd,
+  isFadeOut,
+  firstTime,
+) {
+  fadeStepTimeout = setInterval(() => {
+    currFadeStep += 1;
+    opacity +=
+      Math.round(((opacityEnd - opacityStart) / numFadeSteps) * 100) / 100;
+    scale += Math.round(((scaleEnd - scaleStart) / numFadeSteps) * 1000) / 1000;
+    $boxImage.style.opacity = String(opacity);
+    $boxImage.style.transform = `scale(${scale})`;
+    if (currFadeStep === numFadeSteps && fadeStepTimeout) {
+      clearInterval(fadeStepTimeout);
+      fadeStepTimeout = null;
+      currFadeStep = 0;
+      $boxImage.style.opacity = String(opacityEnd);
+      $boxImage.style.transform = `scale(${scaleEnd})`;
+      if (isFadeOut) {
+        $boxImage.setAttribute('src', `images/${currIndex}.png`);
+        setTimeout(() => performFadeIn(false), boxFadeDelay / 3);
+      } else if (firstTime) {
+        $boxImage?.classList.remove('faded-out');
+      }
+    }
+  }, boxFadeDelay / numFadeSteps);
 }
 function getNextIndex() {
-  if (currentIndex === numImages) {
+  if (currIndex === numImages) {
     return 1;
   } else {
-    return currentIndex + 1;
+    return currIndex + 1;
   }
 }
 function getPrevIndex() {
-  if (currentIndex === 1) {
+  if (currIndex === 1) {
     return numImages;
   } else {
-    return currentIndex - 1;
+    return currIndex - 1;
   }
 }
 function handleClick() {
@@ -45,7 +100,8 @@ function handleDblClick() {
   singleClickTimeout = null;
 }
 function handleOnLoad() {
-  showIndex(currentIndex);
+  // showIndex(currIndex);
+  performFadeIn(true);
 }
 const $page = document.querySelector('.page');
 if (!$page) throw new Error('$page is null');
--- ts/main.ts
+++ ts/main.ts
@@ -1,37 +1,97 @@
 const numImages = 10;
-const boxFadeDelay = 500;
+const boxFadeDelay = 350;
 // Adjust delay to match typical double-click speed:
 const singleClickDelay = 250;
 
-const $boxImage = document.querySelector('.box');
+const $boxImage: HTMLElement | null = document.querySelector('.box');
 if (!$boxImage) throw new Error('Image not found!');
 
-let currentIndex = 1;
+let currIndex = 1;
 let singleClickTimeout: NodeJS.Timeout | null = null;
+let fadeStepTimeout: NodeJS.Timeout | null = null;
+
+const numFadeSteps = 25;
+let currFadeStep = 0;
+const opacityMax = 1;
+const opacityMin = 0;
+let opacity = opacityMax;
+const scaleMax = 1.05;
+const scaleMin = 1;
+let scale = 1;
 
 function showIndex(targetIndex: number): void {
   console.log(targetIndex);
-  currentIndex = targetIndex;
+  currIndex = targetIndex;
+  /*
   $boxImage?.classList.add('faded-out');
   setTimeout(() => {
-    $boxImage!.setAttribute('src', `images/${currentIndex}.png`);
+    $boxImage!.setAttribute('src', `images/${currIndex}.png`);
     $boxImage?.classList.remove('faded-out');
   }, boxFadeDelay);
+  */
+
+  performFadeOut();
+}
+
+function performFadeOut(): void {
+  performFadeInOut(opacityMax, opacityMin, scaleMin, scaleMax, true, false);
+}
+
+function performFadeIn(firstTime: boolean): void {
+  performFadeInOut(
+    opacityMin,
+    opacityMax,
+    scaleMax,
+    scaleMin,
+    false,
+    firstTime,
+  );
+}
+
+function performFadeInOut(
+  opacityStart: number,
+  opacityEnd: number,
+  scaleStart: number,
+  scaleEnd: number,
+  isFadeOut: boolean,
+  firstTime: boolean,
+): void {
+  fadeStepTimeout = setInterval(() => {
+    currFadeStep += 1;
+    opacity +=
+      Math.round(((opacityEnd - opacityStart) / numFadeSteps) * 100) / 100;
+    scale += Math.round(((scaleEnd - scaleStart) / numFadeSteps) * 1000) / 1000;
+    $boxImage!.style.opacity = String(opacity);
+    $boxImage!.style.transform = `scale(${scale})`;
+    if (currFadeStep === numFadeSteps && fadeStepTimeout) {
+      clearInterval(fadeStepTimeout);
+      fadeStepTimeout = null;
+      currFadeStep = 0;
+      $boxImage!.style.opacity = String(opacityEnd);
+      $boxImage!.style.transform = `scale(${scaleEnd})`;
+      if (isFadeOut) {
+        $boxImage!.setAttribute('src', `images/${currIndex}.png`);
+        setTimeout(() => performFadeIn(false), boxFadeDelay / 3);
+      } else if (firstTime) {
+        $boxImage?.classList.remove('faded-out');
+      }
+    }
+  }, boxFadeDelay / numFadeSteps);
 }
 
 function getNextIndex(): number {
-  if (currentIndex === numImages) {
+  if (currIndex === numImages) {
     return 1;
   } else {
-    return currentIndex + 1;
+    return currIndex + 1;
   }
 }
 
 function getPrevIndex(): number {
-  if (currentIndex === 1) {
+  if (currIndex === 1) {
     return numImages;
   } else {
-    return currentIndex - 1;
+    return currIndex - 1;
   }
 }
 
@@ -52,7 +112,8 @@ function handleDblClick(): void {
 }
 
 function handleOnLoad(): void {
-  showIndex(currentIndex);
+  // showIndex(currIndex);
+  performFadeIn(true);
 }
 
 const $page = document.querySelector('.page');
